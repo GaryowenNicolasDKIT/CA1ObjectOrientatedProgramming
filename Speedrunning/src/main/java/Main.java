dec45d5a415ad534e8bb7c8431a365e3a6e3f348
GaryowenNicolasDKIT <d00284800@student.dkit.ie>
2025-10-22 10:23:26 +0100

    Commit 2
    Added ToString method, Date and DateTime values as well as a Constructor that takes said values.

--- Speedrunning/src/main/java/Main.java
+++ Speedrunning/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,9 +9,8 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
         Random r = new Random();
-
         Runner r1 = new Runner("John", "Mario", 1, 0.00001, false);
-
+        System.out.println(r1.toString());
     }
 
 }
--- Speedrunning/src/main/java/Runner.java
+++ Speedrunning/src/main/java/Runner.java
@@ -1,19 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Runner {
     private String Name;
     private String Game;
-    private int Runs_Ammount;
+    private int Runs_Amount;
     private double Community_Rating;
-    private boolean Recrod_or_Not;
-    //private LocalDate Join_Date;
-    //private LocalDateTime LastSubbimission;
+    private boolean Has_World_Record;
+    private LocalDate Join_Date;
+    private LocalDateTime LastSubmission;
 
 
-    public Runner(String name, String game, int runs_Ammount, double community_Rating, boolean recrod_or_Not) {
+    public Runner(String name, String game, int runs_Amount, double community_Rating, boolean has_world_Record, LocalDate join_date, LocalDateTime last_submission) {
         Name = name;
         Game = game;
-        Runs_Ammount = runs_Ammount;
+        Runs_Amount = runs_Amount;
         Community_Rating = community_Rating;
-        Recrod_or_Not = recrod_or_Not;
+        Has_World_Record = has_world_Record;
+        Join_Date = join_date;
+        LastSubmission = last_submission;
+    }
+
+    public Runner(String name, String game, int runs_Amount, double community_Rating, boolean has_world_Record) {
+        Name = name;
+        Game = game;
+        Runs_Amount = runs_Amount;
+        Community_Rating = community_Rating;
+        Has_World_Record = has_world_Record;
+        Join_Date = LocalDate.now();
+        LastSubmission = LocalDateTime.now();
     }
 
     public String getName() {
@@ -24,6 +40,7 @@ public class Runner {
         Name = name;
     }
 
+
     public String getGame() {
         return Game;
     }
@@ -32,13 +49,15 @@ public class Runner {
         Game = game;
     }
 
-    public int getRuns_Ammount() {
-        return Runs_Ammount;
+
+    public int getRuns_Amount() {
+        return Runs_Amount;
     }
 
-    public void setRuns_Ammount(int runs_Ammount) {
-        Runs_Ammount = runs_Ammount;
+    public void setRuns_Amount(int runs_Amount) {
+        Runs_Amount = runs_Amount;
     }
+
 
     public double getCommunity_Rating() {
         return Community_Rating;
@@ -48,11 +67,25 @@ public class Runner {
         Community_Rating = community_Rating;
     }
 
-    public boolean isRecrod_or_Not() {
-        return Recrod_or_Not;
+
+    public boolean isHas_World_Record() {
+        return Has_World_Record;
     }
 
-    public void setRecrod_or_Not(boolean recrod_or_Not) {
-        Recrod_or_Not = recrod_or_Not;
+    public void setHas_World_Record(boolean has_World_Record) {
+        Has_World_Record = has_World_Record;
+    }
+
+
+    public LocalDate getJoinDate() { return Join_Date; }
+
+    public void setJoinDate(LocalDate joinDate) { Join_Date = joinDate; }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.Name + "\nGame: " + this.Game + "\nRuns Amount: "
+                + this.Runs_Amount + "\nCommunity Rating: " + this.Community_Rating
+                + "\nHas World Record: " + this.Has_World_Record +  "\nJoin Date: " + this.Join_Date
+                + "\nLast Submission: " + this.LastSubmission;
     }
 }
